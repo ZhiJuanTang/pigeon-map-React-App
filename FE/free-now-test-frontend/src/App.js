8318eaee578ec57381b524d82fadf28c728a889a
ZhiJuanTang <tangzhijuan56@gmail.com>
2022-04-13 20:59:12 +0200

    pigeon-map of cars in Hamburg

--- FE/free-now-test-frontend/src/App.js
+++ FE/free-now-test-frontend/src/App.js
@@ -1,36 +1,36 @@
 import { useEffect, useState } from 'react';
-import './App.css';
+// import './App.css';
 import axios from "axios";
 import MapMainFreenow from './components/MapMainFreenow';
 
 const App =()=>{
   const [cars, setCars] = useState([]);
-  const [loading, setLoading] = useState(false);
- 
+  // const [loading, setLoading] = useState(false);
+  
   useEffect(() => {
     const getCars = async () => {
       try {
-        setLoading(true);
+        // setLoading(true);
  const {data} = await axios.get(
    "http://localhost:5000/free-now/vehicles"
  );
-setCars(data);
-console.log(data)
-setLoading(false);
+setCars(data.poiList);
+// console.log(cars)
+// setLoading(false);
       }catch(error){
         return alert("Sorry, no data")
       }
     };
-    getCars();
+    getCars();   
   },[]);
-
+ 
   return (
     <div>
-{loading?(
+{/* {loading?(
 <h3>Loading...</h3>
-) : (
+) : ( */}
   <MapMainFreenow cars={cars} />
-)}
+{/* )} */}
  </div>
   )
 }
--- FE/free-now-test-frontend/src/components/MapMainFreenow.js
+++ FE/free-now-test-frontend/src/components/MapMainFreenow.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Map, Marker } from "pigeon-maps";
 
-const MapMainFreenow = () => {
+const MapMainFreenow = ({ cars }) => {
   return (
-    <div>p</div>
-  )
-}
+    <div className="mapContainer">
+      <Map height={500} defaultCenter={[53.5532316, 10.0087783]} defaultZoom={11}>
+        {cars?.map((car) => {
+          const lat = car.coordinate.latitude;
+          const lon = car.coordinate.longitude;
+          // console.log(car.id);
+          return <Marker width={50} anchor={[lat, lon]} key={car.id} />;
+        })}
+      </Map>
+    </div>
+  );
+};
 
 export default MapMainFreenow;
